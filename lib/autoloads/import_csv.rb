e51dc20784b989568d5f500de4d9ef25780ea9e9
takuya_endu <takuya.e.0702@gmail.com>
2020-12-25 14:23:29 +0900

    import_csv.rbの修正とrails db:seed にてデータインポートの確認

--- lib/autoloads/import_csv.rb
+++ lib/autoloads/import_csv.rb
@@ -2,8 +2,8 @@ class ImportCsv
   def self.import(path)
       CSV.foreach(path, headers: true) do |row|
         Text.create!(
-          genre: row["genre"]
-          title: row["title"]
+          genre: row["genre"],
+          title: row["title"],
           content: row["content"]
         )
       end

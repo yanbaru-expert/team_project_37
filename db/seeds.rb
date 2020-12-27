65b9846f7ff26a01d7b1e8b1f18e03d328905ba1
SonomuraHiroyuki <abc332554331@gmail.com>
2020-12-27 17:53:54 +0900

    deviseを実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,12 @@ ImportCsv.text_data
 
 Movie.destroy_all
 ImportCsv.movie_data
+
+EMAIL = 'test@example.com'
+PASSWORD = 'password'
+
+# テストユーザーが存在しないときだけ作成
+User.find_or_create_by!(email: EMAIL) do |user|
+  user.password = PASSWORD
+  puts 'ユーザーの初期データインポートに成功しました。'
+end

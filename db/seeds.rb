f3883ac930d59c6b11d1f5fef8352077d132a12b
asahi <asap1017@docomo.ne.jp>
2021-01-05 18:30:07 +0900

    seed修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,9 @@ User.find_or_create_by!(email: EMAIL) do |user|
   user.password = PASSWORD
   puts 'ユーザーの初期データインポートに成功しました。'
 end
+
+AdminUser.find_or_create_by!(email: EMAIL) do |admin_user|
+  admin_user.password = PASSWORD 
+  admin_user.password_confirmation = PASSWORD 
+  puts 'adminユーザーの初期データインポートに成功しました。'
+end

8dca6dde862f460dc0b63c8758612b422efc2f6b
asahi <asap1017@docomo.ne.jp>
2021-01-05 22:38:39 +0900

    admin用のメールアドレスを定義

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@ Movie.destroy_all
 ImportCsv.movie_data
 
 EMAIL = 'test@example.com'
+ADMIN_EMAIL = 'admin@example.com'
 PASSWORD = 'password'
 
 # テストユーザーが存在しないときだけ作成
@@ -13,8 +14,7 @@ User.find_or_create_by!(email: EMAIL) do |user|
   puts 'ユーザーの初期データインポートに成功しました。'
 end
 
-AdminUser.find_or_create_by!(email: EMAIL) do |admin_user|
+AdminUser.find_or_create_by!(email: ADMIN_EMAIL) do |admin_user|
   admin_user.password = PASSWORD 
-  admin_user.password_confirmation = PASSWORD 
   puts 'adminユーザーの初期データインポートに成功しました。'
 end

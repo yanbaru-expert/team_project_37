9c51139dfefcf0ff671db14ed20057adbdd3ccde
asahi <asap1017@docomo.ne.jp>
2021-01-07 16:24:29 +0900

    ジャンル指定、ナビバーに一覧ページへのリンクを追加

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -2,6 +2,8 @@ class TextsController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    @texts = Text.all
+    @texts = Text.where(genre: ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"])
   end
+
+  
 end

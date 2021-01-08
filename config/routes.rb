6b41cc7dd1824c7f04f751c420187416c06c053f
asahi <asap1017@docomo.ne.jp>
2021-01-08 11:02:20 +0900

    ルーティングを限定

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "texts#index"
 
-  resources :texts 
+  resources :texts ,only: [:index, :show]
 end
